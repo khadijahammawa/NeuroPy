c5b85ba0dc9612c621698399cb859020f01c9f57
khadijahammawa <104446732+khadijahammawa@users.noreply.github.com>
2022-09-23 17:00:54 -0400

    Update pycom.py

--- pycom.py
+++ pycom.py
@@ -1,20 +1,3 @@
-class DeID:
-    def __init__(self, src_dir, id_dir_name):
-        self.basepath = src_dir
-        self.id_dir = id_dir_name
-
-        def rm_id_dirs(basepath, id_dir):
-            try:
-                for root, dirs, files in os.walk(basepath):
-                    for dir in dirs:
-                        if any (id_dir in dir for i in dirs) == True:
-                            dir_path = os.path.join(root,dir)
-                            shutil.rmtree(dir_path)
-                            print('Sensitive folder(s) removed.')
-            except:
-                raise Exception(f'{id_dir} not found in {dir_path}')
-
-
 class Pycom:
     def __init__(self, root_dir, header_dict, file, extract_path, fname_length):
         self.basepath = root_dir
